2b877f44ab5a9173227b9ca70cdfe19bba2d258c
meljum <melisjumabek58@gmail.com>
2022-12-23 19:31:53 +0600

    fixed app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
-// import Videos from './components/Videos'
 
 function App() {
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element={<HomePage />} />
-        {/* <Route path='/video/:id' element={<Videos />} /> */}
       </Routes>
     </div>
   )
